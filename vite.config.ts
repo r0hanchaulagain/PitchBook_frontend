e4416aa562d7eaf28aeeb316c550538f1afdc28f
RohaN <86584369+r0hanchaulagain@users.noreply.github.com>
2025-07-29 20:52:00 +0545

    https enabled and source map disabled

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,23 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import Inspect from "vite-plugin-inspect";
+import fs from "fs";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), Inspect()],
+  build: {
+    sourcemap: false,
+  },
   server: {
     port: 3000,
     host: true,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "certs/server.key")),
+      cert: fs.readFileSync(path.resolve(__dirname, "certs/server.crt")),
+    },
     hmr: {
-      protocol: "ws",
+      protocol: "wss",
       host: "localhost",
       port: 3000,
     },
